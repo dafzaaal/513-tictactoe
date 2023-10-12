671ed58a1aa5caffaac0c41bf40330ce6bdf8b7a
Dawood Afzaal <dawoodafzaal13@gmail.com>
2023-10-11 20:09:01 -0600

    added skeleton structure for JS code, added comments outlining what needs to be done

---
+++ index.js
@@ -0,0 +1,41 @@
+/* 
+    Course: SENG 513
+    Name and UCID: Dawood Afzaal (UCID: 30063067)
+    Date: Oct. 16th, 2023
+*/
+
+// Save refs to the DOM Nodes, grab tile information using document.querySelector
+
+
+
+//Initialize variables to control the game (ex. board, current player, win status, etc.)
+
+
+
+//Hard code i.e. identify the positions of each board
+
+
+
+//Add utility functions for checking win condtions, some utility functions for example
+
+    /* 
+        - check if player action is valid, i.e. is the tile taken or not
+        - check for a win condition, meaning did a player get a three in a row/col?
+        - change player
+        - announce who won
+    */
+
+//create a function to handle result validation
+
+
+//create function to handle user action
+
+//create a method to handle the reset of the board
+
+//handle logic for custom game components
+
+/* 
+    - for the assignment specs, we need to create atleast 2-3 custom interactions, for this game I am thinking of adding custom buttons which changes the gameplay mechanic
+      (ex. not set in stone but maybe by clicking one button, it removes a the input from any random tile consuming a players turn but can be used as a gamble mechanic to perhaps
+        prevent a user from getting a three in a row)
+*/
